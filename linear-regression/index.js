8a56283ade34210cb040095918fd1d3e3574b551
Sunny Gupta <flameheadsg@gmail.com>
2018-12-12 09:58:00 -0800

    refactored project structure

--- linear-regression/index.js
+++ linear-regression/index.js
@@ -1,9 +1,9 @@
 require('@tensorflow/tfjs-node');
 const tf = require('@tensorflow/tfjs');
-const loadCSV = require('./load-csv');
+const loadCSV = require('../load-csv');
 const LinearRegression = require('./linear-regression');
 
-let { features, labels, testFeatures, testLabels } = loadCSV('./cars.csv', {
+let { features, labels, testFeatures, testLabels } = loadCSV('../data/cars.csv', {
   shuffle: true,
   splitTest: 50,
   dataColumns: ['horsepower', 'weight', 'displacement'],
